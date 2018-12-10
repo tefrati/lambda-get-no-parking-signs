257c7a5d841d0706459257896dd2b5b7f02ad305
Tzahi Efrati <tzahi.efrati@gmail.com>
2018-12-10 11:43:22 -0500

    minor syntax bug fix

--- db.js
+++ db.js
@@ -7,9 +7,9 @@ const insertSigns = async (currentSigns, futureSigns) => {
 		client = await MongoClient.connect (process.env.DB_CONNECTION, { useNewUrlParser: true })
 		let db = client.db(process.env.HobokenParking)
 		let currentCollection = await db.collection(process.env.DB_CURRENT_LOCATION_COLLECTION)
-		await currentCollection.insertOne( { fetchDate: new Date(Date.now()).toISOString(), {signs: currentSigns} } )
+		await currentCollection.insertOne( { fetchDate: new Date(Date.now()).toISOString(), signs: currentSigns } )
 		let futureCollection = await db.collection(process.env.DB_FUTURE_LOCATION_COLLECTION)
-		await futureCollection.insertOne( { fetchDate: new Date(Date.now()).toISOString(), {signs: futureSigns} } )
+		await futureCollection.insertOne( { fetchDate: new Date(Date.now()).toISOString(), signs: futureSigns } )
 	}
 	catch (err) {
 		console.log(`couldn't connect or insert to DB. Err=${err}`)
